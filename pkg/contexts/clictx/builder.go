mandelsoft/ocmorig

setup modes for context builder

pkg/contexts/clictx/builder.go
@@ -54,6 +54,6 @@ func WithInput(r io.Reader) core2.Builder {
 	return core2.Builder{}.WithInput(r)
 }
 
-func New() core2.Context {
-	return core2.Builder{}.New()
+func New(m ...datacontext.BuilderMode) core2.Context {
+	return core2.Builder{}.New(m...)
 }

pkg/contexts/ocm/builder.go
@@ -18,6 +18,7 @@ import (
 	"context"
 
 	"github.com/open-component-model/ocm/pkg/contexts/credentials"
+	"github.com/open-component-model/ocm/pkg/contexts/datacontext"
 	"github.com/open-component-model/ocm/pkg/contexts/oci"
 	"github.com/open-component-model/ocm/pkg/contexts/ocm/core"
 )
@@ -54,6 +55,6 @@ func WithBlobDigesters(reg BlobDigesterRegistry) core.Builder {
 	return core.Builder{}.WithBlobDigesters(reg)
 }
 
-func New() Context {
-	return core.Builder{}.New()
+func New(m ...datacontext.BuilderMode) Context {
+	return core.Builder{}.New(m...)
 }

open-component-model/ocm

pkg/contexts/clictx/builder.go
@@ -53,6 +53,6 @@ func WithInput(r io.Reader) core2.Builder {
 	return core2.Builder{}.WithInput(r)
 }
 
-func New() core2.Context {
-	return core2.Builder{}.New()
+func New(mode ...datacontext.BuilderMode) core2.Context {
+	return core2.Builder{}.New(mode...)
 }

pkg/contexts/ocm/builder.go
@@ -18,6 +18,7 @@ import (
 	"context"
 
 	"github.com/open-component-model/ocm/pkg/contexts/credentials"
+	"github.com/open-component-model/ocm/pkg/contexts/datacontext"
 	"github.com/open-component-model/ocm/pkg/contexts/oci"
 	"github.com/open-component-model/ocm/pkg/contexts/ocm/core"
 )
@@ -54,6 +55,6 @@ func WithBlobDigesters(reg BlobDigesterRegistry) core.Builder {
 	return core.Builder{}.WithBlobDigesters(reg)
 }
 
-func New() Context {
-	return core.Builder{}.New()
+func New(mode ...datacontext.BuilderMode) Context {
+	return core.Builder{}.New(mode...)
 }