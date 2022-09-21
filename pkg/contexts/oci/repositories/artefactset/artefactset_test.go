74a93811b937016ebd0c8a5fc765311f75d83dfe
Uwe Krueger <uwe.krueger@sap.com>
2022-09-21 14:57:07 +0200

    rebase and format

--- pkg/contexts/oci/repositories/artefactset/artefactset_test.go
+++ pkg/contexts/oci/repositories/artefactset/artefactset_test.go
@@ -21,9 +21,8 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	. "github.com/open-component-model/ocm/pkg/contexts/oci/repositories/ctf/testhelper"
-
 	. "github.com/open-component-model/ocm/pkg/contexts/oci/repositories/artefactset/testhelper"
+	. "github.com/open-component-model/ocm/pkg/contexts/oci/repositories/ctf/testhelper"
 
 	"github.com/mandelsoft/vfs/pkg/osfs"
 	"github.com/mandelsoft/vfs/pkg/vfs"
