f07cea1ca7ff7e33f2ffc9fd7ed995fa673ae5a3
nikimanoledaki <niki@weave.works>
2022-10-19 11:03:21 +0200

    Update licensing information headers
    * Update licensing information in headers to correct copyright and Apache 2.0 license for all *.go and *.sh files

--- pkg/contexts/oci/ociutils/helm/sympath/walk_test.go
+++ pkg/contexts/oci/ociutils/helm/sympath/walk_test.go
@@ -1,22 +1,6 @@
-/*
-Copyright (c) for portions of walk_test.go are held by The Go Authors, 2009 and are
-provided under the BSD license.
-
-https://github.com/golang/go/blob/master/LICENSE
-
-Copyright The Helm Authors.
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+// SPDX-FileCopyrightText: 2022 SAP SE or an SAP affiliate company and Open Component Model contributors.
+//
+// SPDX-License-Identifier: Apache-2.0
 
 package sympath
 
