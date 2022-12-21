91127c9a520b53461c074a40753016ea86e06311
Uwe Krueger <uwe.krueger@sap.com>
2022-12-21 16:00:56 +0100

    builtin support for GitHub token (#224)

--- pkg/contexts/credentials/init.go
+++ pkg/contexts/credentials/init.go
@@ -5,6 +5,7 @@
 package credentials
 
 import (
+	_ "github.com/open-component-model/ocm/pkg/contexts/credentials/builtin"
 	_ "github.com/open-component-model/ocm/pkg/contexts/credentials/config"
 	_ "github.com/open-component-model/ocm/pkg/contexts/credentials/repositories"
 )
