1470382629b76944f91974ff918b4b08b3a6f9ce
Niki <niki@weave.works>
2023-01-16 10:24:14 +0100

    Bootstrap release process (#233)
    * Add release process
    
    * Add GA workflow to trigger release
    * Change process for track and print version
    * Add script for release process
    
    * Add test release notes
    
    Add test release notes
    
    * Configure goreleaser
    
    * Print version from go cmd
    
    * Release 0.2.0-dev
    
    * Set github actions bot email
    
    * Move steps from script to workflow
    
    * Fix linter
    
    * Add exception for linter
    
    * Update pkg/version/generate/release_generate.go
    
    Co-authored-by: Gergely Brautigam <182850+Skarlso@users.noreply.github.com>

---
+++ pkg/version/generate/release_generate.go
@@ -0,0 +1,29 @@
+// SPDX-FileCopyrightText: 2022 SAP SE or an SAP affiliate company and Open Component Model contributors.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/Masterminds/semver/v3"
+
+	"github.com/open-component-model/ocm/pkg/version"
+)
+
+func main() {
+	if len(os.Args) <= 1 {
+		log.Fatal("missing argument")
+	}
+
+	_ = semver.MustParse(version.ReleaseVersion)
+
+	cmd := os.Args[1]
+	//nolint:forbidigo // Logger not needed for this command.
+	if cmd == "print-version" {
+		fmt.Print(version.ReleaseVersion)
+	}
+}
---
+++ pkg/version/release.go
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: 2022 SAP SE or an SAP affiliate company and Open Component Model contributors.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package version
+
+// ReleaseVersion is the version number in semver format X.Y.Z.
+var ReleaseVersion = "v0.2.0-dev"
