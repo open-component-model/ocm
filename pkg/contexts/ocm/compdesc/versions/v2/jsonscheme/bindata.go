11fff7719e4385bcc6ee4ff073fef652e588ce9e
Uwe Krueger <uwe.krueger@sap.com>
2023-03-10 13:18:37 +0100

    update generated files (#293)

--- pkg/contexts/ocm/compdesc/versions/v2/jsonscheme/bindata.go
+++ pkg/contexts/ocm/compdesc/versions/v2/jsonscheme/bindata.go
@@ -94,7 +94,7 @@ func ResourcesComponentDescriptorV2SchemaYaml() (*asset, error) {
 		return nil, err
 	}
 
-	info := bindataFileInfo{name: "../../../../../../../resources/component-descriptor-v2-schema.yaml", size: 10448, mode: os.FileMode(420), modTime: time.Unix(1675519154, 0)}
+	info := bindataFileInfo{name: "../../../../../../../resources/component-descriptor-v2-schema.yaml", size: 10448, mode: os.FileMode(436), modTime: time.Unix(1678441755, 0)}
 	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
