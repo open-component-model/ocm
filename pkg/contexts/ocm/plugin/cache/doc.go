4620ea5ca16d87eee79ce0eab0a0c3013fc9b95c
Fabian Burth <fabian.burth@sap.com>
2023-06-14 15:55:40 +0200

    adjust wording in documentation

--- pkg/contexts/ocm/plugin/cache/doc.go
+++ pkg/contexts/ocm/plugin/cache/doc.go
@@ -3,5 +3,5 @@
 // SPDX-License-Identifier: Apache-2.0
 
 // Package cache implements the plugin cache that contains all loaded plugins. It keeps the metadata for the various
-// found plugins and provides a Go abstraction to call the appropriate commands.
+// discovered plugins and provides a Go abstraction to call the appropriate commands.
 package cache
