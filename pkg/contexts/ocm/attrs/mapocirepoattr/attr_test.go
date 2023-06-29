7c2f84717b1d38f4d3c74f4960eec8f333bce61b
Uwe Krueger <uwe.krueger@sap.com>
2023-06-29 15:52:23 +0200

    introduce mapocirepoattr to control OCI artifact upload (#396)
    * mapocirepoattr to control OCI artifact upload
    
    * Correct Typo
    
    ---------
    
    Co-authored-by: Gerald Morrison <67469729+morri-son@users.noreply.github.com>

---
+++ pkg/contexts/ocm/attrs/mapocirepoattr/attr_test.go
@@ -0,0 +1,39 @@
+// SPDX-FileCopyrightText: 2022 SAP SE or an SAP affiliate company and Open Component Model contributors.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package mapocirepoattr_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/open-component-model/ocm/pkg/contexts/datacontext"
+	me "github.com/open-component-model/ocm/pkg/contexts/ocm/attrs/mapocirepoattr"
+)
+
+var _ = Describe("attribute", func() {
+	var ctx datacontext.Context
+
+	BeforeEach(func() {
+		ctx = datacontext.New(nil)
+	})
+
+	It("set bool", func() {
+		Expect(me.Get(ctx)).To(Equal(&me.Attribute{Mode: me.NoneMode}))
+		ctx.GetAttributes().SetAttribute(me.ATTR_KEY, true)
+		a := me.Get(ctx)
+		Expect(a).To(Equal(&me.Attribute{Mode: me.ShortHashMode}))
+		hash := "5afa3f0f1b63d64422e7f93e2d9792b7c1f3b4462a931d80b25703f7e6fc79c2"
+		Expect(a.Map("very-long-path/with-many-path-segments/and-really-longer-than-a-hash/artifact")).To(Equal(hash[:8] + "/artifact"))
+	})
+
+	It("set attr", func() {
+		ctx.GetAttributes().SetAttribute(me.ATTR_KEY, &me.Attribute{Mode: me.MappingMode, PrefixMappings: map[string]string{"a": "b", "a/b": "c"}})
+		a := me.Get(ctx)
+		Expect(a).To(Equal(&me.Attribute{Mode: me.MappingMode, PrefixMappings: map[string]string{"a": "b", "a/b": "c"}}))
+		Expect(a.Map("a/b/c")).To(Equal("c/c"))
+		Expect(a.Map("a/c")).To(Equal("b/c"))
+		Expect(a.Map("x/y")).To(Equal("x/y"))
+	})
+})
