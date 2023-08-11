6345f041af05fc845162e26f9acb06cc81fe35b1
Uwe Krueger <uwe.krueger@sap.com>
2023-08-11 16:32:46 +0200

    extend utf8 input type to accept and generate YAML/JSON content (#487)

--- cmds/ocm/commands/ocmcmds/common/inputs/types/utf8/cli.go
+++ cmds/ocm/commands/ocmcmds/common/inputs/types/utf8/cli.go
@@ -11,12 +11,16 @@ import (
 )
 
 func ConfigHandler() flagsets.ConfigOptionTypeSetHandler {
-	set := flagsets.NewConfigOptionTypeSetHandler(TYPE, AddConfig, options.TextOption)
+	set := flagsets.NewConfigOptionTypeSetHandler(TYPE, AddConfig,
+		options.TextOption, options.JSONOption, options.FormattedJSONOption, options.YAMLOption)
 	cpi.AddProcessSpecOptionTypes(set)
 	return set
 }
 
 func AddConfig(opts flagsets.ConfigOptions, config flagsets.Config) error {
 	flagsets.AddFieldByOptionP(opts, options.TextOption, config, "text")
+	flagsets.AddFieldByOptionP(opts, options.JSONOption, config, "json")
+	flagsets.AddFieldByOptionP(opts, options.FormattedJSONOption, config, "formattedJson")
+	flagsets.AddFieldByOptionP(opts, options.YAMLOption, config, "yaml")
 	return cpi.AddProcessSpecConfig(opts, config)
 }
