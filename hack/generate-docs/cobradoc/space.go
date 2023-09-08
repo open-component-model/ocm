3204008dd4fa0fe903b50e264db31444b41e1d79
Uwe Krueger <uwe.krueger@sap.com>
2023-09-08 09:55:12 +0200

    fix trailing space handling for write sequences (#500)

--- hack/generate-docs/cobradoc/space.go
+++ hack/generate-docs/cobradoc/space.go
@@ -19,21 +19,30 @@ type trimWriter struct {
 }
 
 func (w *trimWriter) Write(p []byte) (n int, err error) {
+	start := 0
 	if w.spaces > 0 {
+	loop:
 		for i := 0; i < len(p); i++ {
-			if p[i] != ' ' {
-				_, err := w.Writer.Write([]byte(fmt.Sprintf("%#s", w.spaces, " ")))
+			switch p[i] {
+			case ' ':
+				w.spaces++
+				start++
+			case '\n':
+				// discard spaces
+				w.spaces = 0
+				break loop
+			default:
+				_, err := w.Writer.Write([]byte(fmt.Sprintf(fmt.Sprintf("%%%ds", w.spaces), " ")))
 				if err != nil {
 					return 0, err
 				}
 				w.spaces = 0
-				break
+				break loop
 			}
 		}
 	}
 
-	start := 0
-	for i := 0; i < len(p); i++ {
+	for i := start; i < len(p); i++ {
 		b := p[i]
 		if IsASCIIRune(b) {
 			switch b {
@@ -43,10 +52,9 @@ func (w *trimWriter) Write(p []byte) (n int, err error) {
 				if w.spaces > 0 {
 					// discard spaces
 					written, err := w.Writer.Write(p[start : i-w.spaces])
-					n += written + w.spaces
 					w.spaces = 0
 					if err != nil {
-						return n, err
+						return start + written, err
 					}
 					start = i
 				}
@@ -57,12 +65,11 @@ func (w *trimWriter) Write(p []byte) (n int, err error) {
 			w.spaces = 0
 		}
 	}
-	if start < len(p)-1 {
-		var written int
-		written, err = w.Writer.Write(p[start:])
-		n += written
+	if start+w.spaces < len(p) {
+		written, err := w.Writer.Write(p[start : len(p)-w.spaces])
+		return start + written + w.spaces, err
 	}
-	return n, err
+	return len(p), nil
 }
 
 // IsASCIIRune reports whether the byte is an ASCII byte and not part of an UTF8 rune
