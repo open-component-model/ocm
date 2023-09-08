0a7b599fad19f59ca9e21864788cac9f3731a6f9
Uwe Krueger <uwe.krueger@sap.com>
2023-09-08 09:55:12 +0200

    fix trailing space handling for write sequences (#500)

---
+++ hack/generate-docs/cobradoc/suite_test.go
@@ -0,0 +1,77 @@
+// SPDX-FileCopyrightText: 2022 SAP SE or an SAP affiliate company and Open Component Model contributors.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package cobradoc_test
+
+import (
+	"bytes"
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/open-component-model/ocm/hack/generate-docs/cobradoc"
+)
+
+var _ = Describe("spaces", func() {
+	It("handles spaces at the end of a write followed by a newline", func() {
+		var buf bytes.Buffer
+		w := cobradoc.OmitTrailingSpaces(&buf)
+
+		Expect(w.Write([]byte("a   "))).To(Equal(4))
+		Expect(w.Write([]byte("\nbc   \n"))).To(Equal(7))
+
+		Expect(buf.String()).To(Equal("a\nbc\n"))
+	})
+	It("handles spaces at the end of a write followed by spaces and a newline", func() {
+		var buf bytes.Buffer
+		w := cobradoc.OmitTrailingSpaces(&buf)
+
+		Expect(w.Write([]byte("a   "))).To(Equal(4))
+		Expect(w.Write([]byte("  \nbc   \n"))).To(Equal(9))
+
+		Expect(buf.String()).To(Equal("a\nbc\n"))
+	})
+	It("handles spaces at the end of a write followed by spaces followed by spaces and a newline", func() {
+		var buf bytes.Buffer
+		w := cobradoc.OmitTrailingSpaces(&buf)
+
+		Expect(w.Write([]byte("a   "))).To(Equal(4))
+		Expect(w.Write([]byte("   "))).To(Equal(3))
+		Expect(w.Write([]byte("  \nbc   \n"))).To(Equal(9))
+
+		Expect(buf.String()).To(Equal("a\nbc\n"))
+	})
+	It("handles spaces at the end of a write followed by intermediate followed by spaces and a newline", func() {
+		var buf bytes.Buffer
+		w := cobradoc.OmitTrailingSpaces(&buf)
+
+		Expect(w.Write([]byte("a   "))).To(Equal(4))
+		Expect(w.Write([]byte(" x "))).To(Equal(3))
+		Expect(w.Write([]byte("  \nbc   \n"))).To(Equal(9))
+
+		Expect(buf.String()).To(Equal("a    x\nbc\n"))
+	})
+	It("handles spaces at the end of a write not followed by a newline", func() {
+		var buf bytes.Buffer
+		w := cobradoc.OmitTrailingSpaces(&buf)
+
+		Expect(w.Write([]byte("a   "))).To(Equal(4))
+		Expect(w.Write([]byte("bc   \n"))).To(Equal(6))
+
+		Expect(buf.String()).To(Equal("a   bc\n"))
+	})
+	It("handles spaces at the end", func() {
+		var buf bytes.Buffer
+		w := cobradoc.OmitTrailingSpaces(&buf)
+
+		Expect(w.Write([]byte("a   "))).To(Equal(4))
+		Expect(buf.String()).To(Equal("a"))
+	})
+})
+
+func TestConfig(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Cobra Doc")
+}
