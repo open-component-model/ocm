55a54444dd3ff2081df214cf3f41d5fd0a08fc8e
Uwe Krueger <uwe.krueger@sap.com>
2023-09-26 13:54:04 +0200

    common implementation for CLI option providing types

--- pkg/contexts/ocm/labels/routingslip/spi/support.go
+++ pkg/contexts/ocm/labels/routingslip/spi/support.go
@@ -5,7 +5,7 @@
 package spi
 
 import (
-	"github.com/open-component-model/ocm/pkg/cobrautils/flagsets"
+	"github.com/open-component-model/ocm/pkg/contexts/ocm/cpi/clitypes"
 	"github.com/open-component-model/ocm/pkg/runtime"
 )
 
@@ -29,81 +29,16 @@ func MustNewEntryMultiFormatVersion(kind string, formats EntryFormatVersionRegis
 
 ////////////////////////////////////////////////////////////////////////////////
 
-type additionalTypeInfo interface {
-	ConfigOptionTypeSetHandler() flagsets.ConfigOptionTypeSetHandler
-	Description() string
-	Format() string
-}
-
-type entryType struct {
-	runtime.VersionedTypedObjectType[Entry]
-	description string
-	format      string
-	handler     flagsets.ConfigOptionTypeSetHandler
-	validator   func(Entry) error
-}
-
-var _ additionalTypeInfo = (*entryType)(nil)
-
-func NewEntryTypeByBaseType(vt runtime.VersionedTypedObjectType[Entry], opts ...EntryTypeOption) EntryType {
-	t := entryTypeTarget{&entryType{
-		VersionedTypedObjectType: vt,
-	}}
-	for _, o := range opts {
-		o.ApplyToEntryOptionTarget(t)
-	}
-	return t.entryType
-}
-
 func NewEntryType[I Entry](name string, opts ...EntryTypeOption) EntryType {
-	return NewEntryTypeByBaseType(runtime.NewVersionedTypedObjectType[Entry, I](name), opts...)
+	return clitypes.NewCLITypedObjectTypeObject[Entry](runtime.NewVersionedTypedObjectType[Entry, I](name), opts...)
 }
 
-func NewAccessSpecTypeByConverter[I Entry, V runtime.VersionedTypedObject](name string, converter runtime.Converter[I, V], opts ...EntryTypeOption) EntryType {
-	return NewEntryTypeByBaseType(runtime.NewVersionedTypedObjectTypeByConverter[Entry, I, V](name, converter), opts...)
+func NewEntryTypeByConverter[I Entry, V runtime.VersionedTypedObject](name string, converter runtime.Converter[I, V], opts ...EntryTypeOption) EntryType {
+	return clitypes.NewCLITypedObjectTypeObject[Entry](runtime.NewVersionedTypedObjectTypeByConverter[Entry, I, V](name, converter), opts...)
 }
 
 func NewEntryTypeByFormatVersion(name string, fmt runtime.FormatVersion[Entry], opts ...EntryTypeOption) EntryType {
-	return NewEntryTypeByBaseType(runtime.NewVersionedTypedObjectTypeByFormatVersion[Entry](name, fmt), opts...)
-}
-
-func (t *entryType) ConfigOptionTypeSetHandler() flagsets.ConfigOptionTypeSetHandler {
-	return t.handler
-}
-
-func (t *entryType) Description() string {
-	return t.description
-}
-
-func (t *entryType) Format() string {
-	return t.format
-}
-
-func (t *entryType) Validate(e Entry) error {
-	if t.validator == nil {
-		return nil
-	}
-	return t.validator(e)
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
-// entryTypeTarget is used as target for option functions, it provides
-// setters for fields, which should nor be modifiable for a final type object.
-type entryTypeTarget struct {
-	*entryType
-}
-
-func (t entryTypeTarget) SetDescription(value string) {
-	t.description = value
-}
-
-func (t entryTypeTarget) SetFormat(value string) {
-	t.format = value
-}
-
-func (t entryTypeTarget) SetConfigHandler(value flagsets.ConfigOptionTypeSetHandler) {
-	t.handler = value
+	return clitypes.NewCLITypedObjectTypeObject[Entry](runtime.NewVersionedTypedObjectTypeByFormatVersion[Entry](name, fmt), opts...)
 }
 
 ////////////////////////////////////////////////////////////////////////////////
