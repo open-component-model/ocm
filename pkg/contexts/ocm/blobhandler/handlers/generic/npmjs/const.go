8177ccceb8742ff9fae2921393bd849437a70992
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2024-02-06 09:17:11 +0100

    gofumpt

--- pkg/contexts/ocm/blobhandler/handlers/generic/npmjs/const.go
+++ pkg/contexts/ocm/blobhandler/handlers/generic/npmjs/const.go
@@ -2,12 +2,15 @@ package npmjs
 
 import "github.com/open-component-model/ocm/pkg/contexts/credentials/cpi"
 
-// CONSUMER_TYPE is the npmjs repository type.
-const CONSUMER_TYPE = "Registry.npmjs.com"
-const BLOB_HANDLER_NAME = "ocm/npmPackage"
-
 const (
+	// CONSUMER_TYPE is the npmjs repository type.
+	CONSUMER_TYPE     = "Registry.npmjs.com"
+	BLOB_HANDLER_NAME = "ocm/npmPackage"
+
+	// ATTR_USERNAME is the username attribute. Required for login at any npmjs registry.
 	ATTR_USERNAME = cpi.ATTR_USERNAME
+	// ATTR_PASSWORD is the password attribute. Required for login at any npmjs registry.
 	ATTR_PASSWORD = cpi.ATTR_PASSWORD
-	ATTR_EMAIL    = cpi.ATTR_EMAIL
+	// ATTR_EMAIL is the email attribute. Required for login at any npmjs registry.
+	ATTR_EMAIL = cpi.ATTR_EMAIL
 )
