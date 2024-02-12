4844aa1114f1c9a1911449559d4a9adf88ba146c
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2024-02-12 14:04:54 +0100

    addition to #633

---
+++ pkg/contexts/ocm/blobhandler/handlers/generic/npm/publish_test.go
@@ -0,0 +1,26 @@
+package npm
+
+import (
+	"encoding/json"
+	"os"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	. "github.com/open-component-model/ocm/pkg/testutils"
+)
+
+var _ = Describe("NPM Publish Test Environment", func() {
+
+	It("prepare package and json marshal", func() {
+		data := Must(os.ReadFile("testdata/testdata.tgz"))
+		pkg := Must(prepare(data))
+		Expect(pkg.Version).To(Equal("0.8.15"))
+		Expect(pkg.Name).To(Equal("testdata"))
+		Expect(pkg.Dist.Integrity).To(Equal("sha512-Dsbnf3b4scCugxBZ+rHm8Hr1CAfyC3h8su31KnPGw21BAkM6X5gbi5Jbci9WaCCBBxm1tMTRKCJqk29j5Aw0gg=="))
+		jsn := Must(json.Marshal(pkg))
+		fixture := `{"name":"testdata","version":"0.8.15","readme":"# Test Data\n\nreadme\n","description":"Test data description.","dist":{"integrity":"sha512-Dsbnf3b4scCugxBZ+rHm8Hr1CAfyC3h8su31KnPGw21BAkM6X5gbi5Jbci9WaCCBBxm1tMTRKCJqk29j5Aw0gg==","shasum":"602b69a43903fa1694d59fefd8cb326cd68e8935","tarball":""}}`
+		Expect(string(jsn)).To(Equal(fixture))
+	})
+
+})
