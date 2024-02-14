b86eab1087e92ff6418817526219747828e35d78
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2024-02-14 13:31:54 +0100

    revert

--- pkg/contexts/credentials/builtin/init.go
+++ pkg/contexts/credentials/builtin/init.go
@@ -1,8 +1,7 @@
+// SPDX-FileCopyrightText: 2022 SAP SE or an SAP affiliate company and Open Component Model contributors.
+//
+// SPDX-License-Identifier: Apache-2.0
+
 package builtin
 
-import (
-	_ "github.com/open-component-model/ocm/pkg/contexts/credentials/builtin/github"
-	_ "github.com/open-component-model/ocm/pkg/contexts/credentials/builtin/helm/identity"
-	_ "github.com/open-component-model/ocm/pkg/contexts/credentials/builtin/npm"
-	_ "github.com/open-component-model/ocm/pkg/contexts/credentials/builtin/oci/identity"
-)
+import _ "github.com/open-component-model/ocm/pkg/contexts/credentials/builtin/github"
