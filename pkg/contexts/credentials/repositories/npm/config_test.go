0702046605f513f6f1b0e7f03c058f6527a7ed29
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2024-02-16 12:20:46 +0100

    fix import cycle in test

--- pkg/contexts/credentials/repositories/npm/config_test.go
+++ pkg/contexts/credentials/repositories/npm/config_test.go
@@ -4,23 +4,10 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
-	"github.com/open-component-model/ocm/pkg/contexts/ocm/blobhandler/handlers/generic/npm"
-	"github.com/open-component-model/ocm/pkg/registrations"
 	. "github.com/open-component-model/ocm/pkg/testutils"
 )
 
 var _ = Describe("Config deserialization Test Environment", func() {
-
-	It("deserializes string", func() {
-		cfg := Must(registrations.DecodeConfig[npm.Config]("test"))
-		Expect(cfg).To(Equal(&npm.Config{Url: "test"}))
-	})
-
-	It("deserializes struct", func() {
-		cfg := Must(registrations.DecodeConfig[npm.Config](`{"Url":"test"}`))
-		Expect(cfg).To(Equal(&npm.Config{Url: "test"}))
-	})
-
 	It("read .npmrc", func() {
 		cfg, err := readNpmConfigFile("testdata/.npmrc")
 		Expect(err).To(BeNil())
---
+++ pkg/contexts/ocm/blobhandler/handlers/generic/npm/registration_test.go
@@ -0,0 +1,23 @@
+package npm_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/open-component-model/ocm/pkg/contexts/ocm/blobhandler/handlers/generic/npm"
+	"github.com/open-component-model/ocm/pkg/registrations"
+	. "github.com/open-component-model/ocm/pkg/testutils"
+)
+
+var _ = Describe("Config deserialization Test Environment", func() {
+
+	It("deserializes string", func() {
+		cfg := Must(registrations.DecodeConfig[npm.Config]("test"))
+		Expect(cfg).To(Equal(&npm.Config{Url: "test"}))
+	})
+
+	It("deserializes struct", func() {
+		cfg := Must(registrations.DecodeConfig[npm.Config](`{"Url":"test"}`))
+		Expect(cfg).To(Equal(&npm.Config{Url: "test"}))
+	})
+})
