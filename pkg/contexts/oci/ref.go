b412a77f1bce7857b79306d1dfa6ab1977e93730
Fabian Burth <fabian.burth@sap.com>
2024-03-04 17:31:57 +0100

    enable parsing of oci reference of format localhost:port/repo:version (or anyname:port/repo:version)

--- pkg/contexts/oci/ref.go
+++ pkg/contexts/oci/ref.go
@@ -160,6 +160,15 @@ func ParseRef(ref string) (RefSpec, error) {
 		spec.Digest = dig(match[5])
 		return spec, nil
 	}
+	match = grammar.TypedGenericReferenceRegexpWithPort.FindSubmatch([]byte(ref))
+	if match != nil {
+		spec.Type = string(match[1])
+		spec.Info = string(match[2])
+		spec.Repository = string(match[3])
+		spec.Tag = pointer(match[4])
+		spec.Digest = dig(match[5])
+		return spec, nil
+	}
 	match = grammar.AnchoredRegistryRegexp.FindSubmatch([]byte(ref))
 	if match != nil {
 		spec.Type = string(match[1])
