0a0915ee467422dda2e5a7653e9ff03689327157
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2024-04-10 16:31:34 +0200

    access method

--- pkg/contexts/ocm/accessmethods/mvn/method.go
+++ pkg/contexts/ocm/accessmethods/mvn/method.go
@@ -4,9 +4,10 @@ import (
 	"bytes"
 	"context"
 	"crypto"
-	"encoding/json"
-	"encoding/xml
+	"encoding/xml"
 	"fmt"
+	npmCredentials "github.com/open-component-model/ocm/pkg/contexts/credentials/builtin/npm/identity"
+	"github.com/open-component-model/ocm/pkg/logging"
 	"io"
 	"net/http"
 	"path"
@@ -35,6 +36,8 @@ func init() {
 	accspeccpi.RegisterAccessType(accspeccpi.NewAccessSpecType[*AccessSpec](Type, accspeccpi.WithDescription(usage)))
 	accspeccpi.RegisterAccessType(accspeccpi.NewAccessSpecType[*AccessSpec](TypeV1, accspeccpi.WithFormatSpec(formatV1), accspeccpi.WithConfigHandler(ConfigHandler())))
 }
+
+var REALM = logging.DefineSubRealm("Maven repository", "mvn")
 
 // AccessSpec describes the access for a Maven (mvn) repository.
 type AccessSpec struct {
@@ -88,83 +91,146 @@ func (a *AccessSpec) AccessMethod(c accspeccpi.ComponentVersionAccess) (accspecc
 }
 
 func (a *AccessSpec) GetInexpensiveContentVersionIdentity(access accspeccpi.ComponentVersionAccess) string {
-	meta, _ := a.getPackageMeta(access.GetContext())
+	meta, _ := a.GetPackageMeta(access.GetContext())
 	if meta != nil {
-		return meta.Dist.Shasum
+		return meta.Hash
 	}
 	return ""
 }
 
-func (a *AccessSpec) getPackageMeta(ctx accspeccpi.Context) (*meta, error) {
-	//    <groupId>cn.afternode.commons</groupId>
-	//    <artifactId>commons</artifactId>
-	//    <version>1.6</version>
-	// repository: https://repo1.maven.org/maven2/
-	// groupId: cn/afternode/commons
-	// version: 1.6
-	// artifactId: commons
-	url := a.Repository + path.Join("/", strings.Replace(a.GroupId, ".", "/", -1), a.ArtifactId, a.Version, a.ArtifactId+"-"+a.Version+".pom")
-	// https://repo1.maven.org/maven2/cn/afternode/commons/commons/1.6/commons-1.6.pom
-	r, err := reader(url, vfsattr.Get(ctx))
+type meta struct {
+	Packaging string      `json:"packaging"`
+	HashType  crypto.Hash `json:"hashType"`
+	Hash      string      `json:"hash"`
+	Asc       string      `json:"asc"`
+	Bin       string      `json:"bin"`
+}
+
+func (a *AccessSpec) GetPackageMeta(ctx accspeccpi.Context) (*meta, error) {
+	// this is how the usual maven repository structure looks like
+	urlPrefix := a.Repository + path.Join("/", strings.Replace(a.GroupId, ".", "/", -1), a.ArtifactId, a.Version, a.ArtifactId+"-"+a.Version+".")
+	fs := vfsattr.Get(ctx)
+
+	// first let's read the pom file and check which binary artifact we need to read
+	log := logging.Context().Logger(npmCredentials.REALM)
+	log.Debug("Reading %s", urlPrefix+"pom")
+	pom, err := readPom(urlPrefix+"pom", fs)
 	if err != nil {
-		return nil, err
-	}
-	buf := &bytes.Buffer{}
-	_, err = io.Copy(buf, io.LimitReader(r, 200000))
-	if err != nil {
-		return nil, errors.Wrapf(err, "cannot get version metadata for %s", url)
+		return nil, errors.Wrapf(err, "cannot read pom file for %s", urlPrefix)
 	}
 
-	var metadata meta
-
-	// read xml from buf and fill metadata
-	xml.Unmarshal(buf.Bytes(), &metadata)
-
-	err = json.Unmarshal(buf.Bytes(), &metadata)
+	hashType, hash, err := getBestHashValue(urlPrefix+pom.Packaging, fs)
 	if err != nil {
-		return nil, errors.Wrapf(err, "cannot unmarshal version metadata for %s", url)
+		log.Debug("Could not find any hash value for %s", urlPrefix+pom.Packaging)
 	}
+	asc, err := getStringData(urlPrefix+pom.Packaging+".asc", fs)
+	if err != nil {
+		log.Debug("No signing info found for %s", urlPrefix+pom.Packaging)
+	}
+
+	var metadata meta = meta{
+		Packaging: pom.Packaging,
+		HashType:  hashType,
+		Hash:      hash,
+		Bin:       urlPrefix + pom.Packaging,
+		Asc:       asc,
+	}
+
 	return &metadata, nil
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
+type project struct {
+	GroupId    string `xml:"groupId"`
+	ArtifactId string `xml:"artifactId"`
+	Version    string `xml:"version"`
+	Packaging  string `xml:"packaging"`
+}
+
+func readPom(url string, fs vfs.FileSystem) (*project, error) {
+	reader, err := getReader(url, fs)
+	if err != nil {
+		return nil, err
+	}
+	buf := &bytes.Buffer{}
+	_, err = io.Copy(buf, io.LimitReader(reader, 200000))
+	if err != nil {
+		return nil, errors.Wrapf(err, "cannot get version metadata for %s", url)
+	}
+	var pom project
+	err = xml.Unmarshal(buf.Bytes(), &pom)
+	if err != nil {
+		return nil, errors.Wrapf(err, "cannot unmarshal version metadata for %s", url)
+	}
+	if pom.Packaging == "" {
+		pom.Packaging = "jar"
+	}
+	return &pom, nil
+}
+
+// getStringData reads all data from the given URL and returns it as a string.
+func getStringData(url string, fs vfs.FileSystem) (string, error) {
+	r, err := getReader(url, fs)
+	if err != nil {
+		return "", err
+	}
+	b, err := io.ReadAll(r)
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
+}
+
+// getBestHashValue returns the best hash value (SHA-512, SHA-256, SHA-1, MD5) for the given artifact (POM, JAR, ...).
+func getBestHashValue(url string, fs vfs.FileSystem) (crypto.Hash, string, error) {
+	arr := [5]crypto.Hash{crypto.SHA512, crypto.SHA256, crypto.SHA1, crypto.MD5}
+	log := logging.Context().Logger(npmCredentials.REALM)
+	for i := 0; i < len(arr); i++ {
+		v, err := getStringData(url+hashUrlExt(arr[i]), fs)
+		if v != "" {
+			return arr[i], v, err
+		}
+		if err != nil {
+			log.Debug("hash value not found: %s - %s", url+hashUrlExt(arr[i]), err)
+		}
+	}
+	return 0, "", errors.New("no hash value found")
+}
+
+// hashUrlExt returns the 'maven' hash extension for the given hash.
+// Maven usually uses sha1, sha256, sha512, md5 instead of SHA-1, SHA-256, SHA-512, MD5.
+func hashUrlExt(h crypto.Hash) string {
+	return "." + strings.ReplaceAll(strings.ToLower(h.String()), "-", "")
+}
+
 func newMethod(c accspeccpi.ComponentVersionAccess, a *AccessSpec) (accspeccpi.AccessMethodImpl, error) {
 	factory := func() (blobaccess.BlobAccess, error) {
-		meta, err := a.getPackageMeta(c.GetContext())
+		meta, err := a.GetPackageMeta(c.GetContext())
 		if err != nil {
 			return nil, err
 		}
 
-		f := func() (io.ReadCloser, error) {
-			return reader(meta.Dist.Tarball, vfsattr.Get(c.GetContext()))
+		reader := func() (io.ReadCloser, error) {
+			return getReader(meta.Bin, vfsattr.Get(c.GetContext()))
 		}
-		if meta.Dist.Shasum != "" {
-			tf := f
-			f = func() (io.ReadCloser, error) {
-				r, err := tf()
+		if meta.Hash != "" {
+			getreader := reader
+			reader = func() (io.ReadCloser, error) {
+				readCloser, err := getreader()
 				if err != nil {
 					return nil, err
 				}
-				return iotools.VerifyingReaderWithHash(r, crypto.SHA1, meta.Dist.Shasum), nil
+				return iotools.VerifyingReaderWithHash(readCloser, meta.HashType, meta.Hash), nil
 			}
 		}
-		acc := blobaccess.DataAccessForReaderFunction(f, meta.Dist.Tarball)
+		acc := blobaccess.DataAccessForReaderFunction(reader, meta.Bin)
 		return accessobj.CachedBlobAccessForWriter(c.GetContext(), mime.MIME_JAR, accessio.NewDataAccessWriter(acc)), nil
 	}
 	return accspeccpi.NewDefaultMethodImpl(c, a, "", mime.MIME_JAR, factory), nil
 }
 
-type meta struct {
-	Type   string `json:"type"` // pom, jar, sources, javadoc, module, ...
-	MD5    string `json:"md5"`
-	Sha1   string `json:"sha1"`
-	Sha256 string `json:"sha256"`
-	Sha512 string `json:"sha512"`
-	Asc    string `json:"asc"`
-}
-
-func reader(url string, fs vfs.FileSystem) (io.ReadCloser, error) {
+func getReader(url string, fs vfs.FileSystem) (io.ReadCloser, error) {
 	c := &http.Client{}
 
 	if strings.HasPrefix(url, "file://") {
--- pkg/contexts/ocm/accessmethods/mvn/method_test.go
+++ pkg/contexts/ocm/accessmethods/mvn/method_test.go
@@ -10,14 +10,14 @@ import (
 	. "github.com/open-component-model/ocm/pkg/testutils"
 
 	"github.com/open-component-model/ocm/pkg/contexts/ocm"
-	"github.com/open-component-model/ocm/pkg/contexts/ocm/accessmethods/mvn" // FIXME: Update import path
+	"github.com/open-component-model/ocm/pkg/contexts/ocm/accessmethods/mvn"
 	"github.com/open-component-model/ocm/pkg/contexts/ocm/cpi"
 	"github.com/open-component-model/ocm/pkg/iotools"
 	"github.com/open-component-model/ocm/pkg/mime"
 )
 
-const mvnPATH = "/testdata/registry"
-const FAILPATH = "/testdata/failregistry"
+const mvnPATH = "/testdata/.m2/repository"
+const FAILPATH = "/testdata"
 
 var _ = Describe("Method", func() {
 	var env *Builder
@@ -32,13 +32,21 @@ var _ = Describe("Method", func() {
 		env.Cleanup()
 	})
 
-	It("accesses artifact", func() {
-		acc := mvn.New("file://"+mvnPATH, "yargs", "17.7.1")
-		// acc := mvn.New("https://registry.mvnjs.org", "yargs", "17.7.1")
+	It("get packaging", func() {
+		acc := mvn.New("https://repo1.maven.org/maven2", "com.sap.cloud.sdk", "sdk-modules-bom", "5.7.0")
+		meta, err := acc.GetPackageMeta(ocm.DefaultContext())
+		Expect(err).ToNot(HaveOccurred())
+		Expect(meta.Packaging).To(Equal("pom"))
+		Expect(meta.Hash).To(Equal("34ccdeb9c008f8aaef90873fc636b09d3ae5c709"))
+		Expect(meta.HashType).To(Equal(crypto.SHA1))
+		Expect(meta.Asc).To(ContainSubstring("-----BEGIN PGP SIGNATURE-----"))
+	})
 
+	It("accesses artifact", func() {
+		acc := mvn.New("file://"+mvnPATH, "com.sap.cloud.sdk", "sdk-modules-bom", "5.7.0")
 		m := Must(acc.AccessMethod(cv))
 		defer m.Close()
-		Expect(m.MimeType()).To(Equal(mime.MIME_TGZ))
+		Expect(m.MimeType()).To(Equal(mime.MIME_JAR)) // FIXME what about POM?
 
 		r := Must(m.Reader())
 		defer r.Close()
@@ -51,15 +59,33 @@ var _ = Describe("Method", func() {
 			}
 		}
 		Expect(dr.Size()).To(Equal(int64(65690)))
-		Expect(dr.Digest().String()).To(Equal("SHA-1:34a77645201d1a8fc5213ace787c220eabbd0967"))
+		Expect(dr.Digest().String()).To(Equal("SHA-1:34ccdeb9c008f8aaef90873fc636b09d3ae5c709"))
 	})
 
 	It("detects digests mismatch", func() {
-		acc := mvn.New("file://"+FAILPATH, "yargs", "17.7.1")
+		acc := mvn.New("file://"+FAILPATH, "fail", "repository", "42")
 
 		m := Must(acc.AccessMethod(cv))
 		defer m.Close()
 		_, err := m.Reader()
 		Expect(err).To(MatchError(ContainSubstring("SHA-1 digest mismatch: expected 44a77645201d1a8fc5213ace787c220eabbd0967, found 34a77645201d1a8fc5213ace787c220eabbd0967")))
 	})
+
+	/* tests are failing with:
+
+	   <*fmt.wrapError | 0xc0008f2240>:
+	   unable to read access: unable to create temporary file: mkdir C:\SAPDEV~1\TEMP\user: lstat /C:: CreateFile C:/SAPDEV~1/TEMP/user/VFS-1551776656/C:: The filename, directory name, or volume label syntax is incorrect.
+	   		msg: "unable to create temporary file: mkdir C:\\SAPDEV~1\\TEMP\\user: lstat /C:: CreateFile C:/SAPDEV~1/TEMP/user/VFS-1551776656/C:: The filename, directory name, or volume label syntax is incorrect."
+	           err: <*fs.PathError | 0xc0008f47e0>{
+	               Op: "mkdir",
+	               Path: "C:\\SAPDEV~1\\TEMP\\user",
+	               Err: <*fs.PathError | 0xc0008f47b0>{
+	                   Op: "lstat",
+	                   Path: "/C:",
+	                   Err: <*fs.PathError | 0xc0008f4780>{
+	                       Op: "CreateFile",
+	                       Path: "C:/SAPDEV~1/TEMP/user/VFS-1551776656/C:",
+	                       Err: <syscall.Errno>0x7b,
+	*/
+
 })
