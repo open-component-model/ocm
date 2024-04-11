54844d3f82e269ca0b1486370d5c60f4c3a21986
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2024-04-11 15:34:32 +0200

    init mvn access

--- pkg/contexts/ocm/accessmethods/init.go
+++ pkg/contexts/ocm/accessmethods/init.go
@@ -1,7 +1,3 @@
-// SPDX-FileCopyrightText: 2022 SAP SE or an SAP affiliate company and Open Component Model contributors.
-//
-// SPDX-License-Identifier: Apache-2.0
-
 package accessmethods
 
 import (
@@ -10,6 +6,7 @@ import (
 	_ "github.com/open-component-model/ocm/pkg/contexts/ocm/accessmethods/localblob"
 	_ "github.com/open-component-model/ocm/pkg/contexts/ocm/accessmethods/localfsblob"
 	_ "github.com/open-component-model/ocm/pkg/contexts/ocm/accessmethods/localociblob"
+	_ "github.com/open-component-model/ocm/pkg/contexts/ocm/accessmethods/mvn"
 	_ "github.com/open-component-model/ocm/pkg/contexts/ocm/accessmethods/none"
 	_ "github.com/open-component-model/ocm/pkg/contexts/ocm/accessmethods/npm"
 	_ "github.com/open-component-model/ocm/pkg/contexts/ocm/accessmethods/ociartifact"
