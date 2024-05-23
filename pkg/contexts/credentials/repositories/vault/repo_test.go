4407d9b3a0da084ed8fcdf03c2efe8cfc4014ef1
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2024-05-23 15:39:15 +0200

    remove SPDX Header

--- pkg/contexts/credentials/repositories/vault/repo_test.go
+++ pkg/contexts/credentials/repositories/vault/repo_test.go
@@ -1,7 +1,3 @@
-// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Open Component Model contributors.
-//
-// SPDX-License-Identifier: Apache-2.0
-
 package vault_test
 
 import (
@@ -11,12 +7,12 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	. "github.com/open-component-model/ocm/pkg/testutils"
 
 	"github.com/open-component-model/ocm/pkg/common"
 	"github.com/open-component-model/ocm/pkg/contexts/credentials"
 	me "github.com/open-component-model/ocm/pkg/contexts/credentials/repositories/vault"
 	"github.com/open-component-model/ocm/pkg/contexts/credentials/repositories/vault/identity"
+	. "github.com/open-component-model/ocm/pkg/testutils"
 )
 
 const (
