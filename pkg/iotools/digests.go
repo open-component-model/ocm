e0cd11a3c3305f5c6caf7f6802b2f3ef68872872
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2024-05-28 16:29:07 +0200

    keep regex - compile only once

--- pkg/iotools/digests.go
+++ pkg/iotools/digests.go
@@ -6,10 +6,12 @@ import (
 	"regexp"
 )
 
+// sha512- or SHA-512:
+var re = regexp.MustCompile(`(?i)^sha(\d+-|\-\d+:)`)
+
 // DecodeBase64ToHex decodes a base64 encoded string and returns the hex representation.
 // Any prefix like 'sha512-' or 'SHA-256:' or 'Sha1-' is removed.
 func DecodeBase64ToHex(b64encoded string) (string, error) {
-	re := regexp.MustCompile(`(?i)^sha(\d+-|\-\d+:)`) // sha512- or SHA-512:
 	b64encoded = re.ReplaceAllString(b64encoded, "")
 	digest, err := base64.StdEncoding.DecodeString(b64encoded)
 	if err != nil {
