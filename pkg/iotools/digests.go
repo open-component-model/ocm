e5e52ac32f5f60618157975dc83ae4d922bb996b
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2024-05-28 12:49:35 +0200

    support of 'sha-1:'

--- pkg/iotools/digests.go
+++ pkg/iotools/digests.go
@@ -6,8 +6,10 @@ import (
 	"regexp"
 )
 
+// DecodeBase64ToHex decodes a base64 encoded string and returns the hex representation.
+// Any prefix like 'sha512-' or 'SHA-256:' or 'Sha1-' is removed.
 func DecodeBase64ToHex(b64encoded string) (string, error) {
-	re := regexp.MustCompile(`(?i)^sha\d+-`)
+	re := regexp.MustCompile(`(?i)^sha(\d+-|\-\d+:)`) // sha512- or SHA-512:
 	b64encoded = re.ReplaceAllString(b64encoded, "")
 	digest, err := base64.StdEncoding.DecodeString(b64encoded)
 	if err != nil {
--- pkg/iotools/digests_test.go
+++ pkg/iotools/digests_test.go
@@ -20,5 +20,9 @@ var _ = Describe("digests.go tests", func() {
 		hex, err = iotools.DecodeBase64ToHex("SHA512-04cXMnFlKzgudf//lH/VqGUtFkplvGv0BCmPREEJsVYTJrxyiBFlsOiZIrjPENBkHWPnK6kOG53VTiqtsILNgw==")
 		Expect(err).To(BeNil())
 		Expect(hex).To(Equal("d387173271652b382e75ffff947fd5a8652d164a65bc6bf404298f444109b1561326bc72881165b0e89922b8cf10d0641d63e72ba90e1b9dd54e2aadb082cd83"))
+
+		hex, err = iotools.DecodeBase64ToHex("Sha-512:04cXMnFlKzgudf//lH/VqGUtFkplvGv0BCmPREEJsVYTJrxyiBFlsOiZIrjPENBkHWPnK6kOG53VTiqtsILNgw==")
+		Expect(err).To(BeNil())
+		Expect(hex).To(Equal("d387173271652b382e75ffff947fd5a8652d164a65bc6bf404298f444109b1561326bc72881165b0e89922b8cf10d0641d63e72ba90e1b9dd54e2aadb082cd83"))
 	})
 })
