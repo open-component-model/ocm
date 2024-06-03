4232ea979ea23eec5fe204fe08092662ac9ac0de
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2024-06-03 17:47:51 +0200

    bump golangci/golangci-lint

--- hack/install-requirements.sh
+++ hack/install-requirements.sh
@@ -5,7 +5,7 @@ PROJECT_ROOT="${CURRENT_DIR}"/..
 
 echo "> Install Go packages/binaries"
 
-curl -sfL "https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh" | sh -s -- -b $(go env GOPATH)/bin v1.54.2
+curl -sfL "https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh" | sh -s -- -b $(go env GOPATH)/bin v1.58.1
 
 GO111MODULE=off go get -u github.com/go-bindata/go-bindata/...
 
