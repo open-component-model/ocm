ec42cb30af895dec37d3c1c1f5b94b5055e29e16
Uwe Krueger <uwe.krueger@sap.com>
2024-06-11 12:54:51 +0200

    fix blobaccess doc.go

--- pkg/blobaccess/blobaccess/doc.go
+++ pkg/blobaccess/blobaccess/doc.go
@@ -3,9 +3,10 @@
 // It is intended to be used by generic BlobAccess users not referring to
 // dedicated implementations.
 // Dedicated implementations can be accessed using the appropriate implementation
-// package. THis separation is required to avoid cycle for BlobAccess implementations
-// using again BlobAccess consumers.
+// package. This separation is required to avoid cycles for BlobAccess implementations
+// using again BlobAccess objects.
 // Alternatively, the parent package can be used, if there are no cycles. The
-// parent package provide thes ebasic functionality plus
-// access the to various implementation flavors in one package.
+// parent package provides the basic functionality plus
+// access to various implementation flavors in one package to offer a
+// simplified discovery of available implementations .
 package blobaccess
