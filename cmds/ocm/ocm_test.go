a85dcf165fcbabc2945ac81f9c615d2041b2cec1
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2024-06-24 15:02:59 +0200

    we can actually specify the ocm cache on the fly:
    cacheattr.Set(env.OCIContext(),
    Must(accessio.NewStaticBlobCache("my-cache-dir")))

--- cmds/ocm/ocm_test.go
+++ cmds/ocm/ocm_test.go
@@ -1,14 +1,18 @@
-//go:build !windows
-
 package main_test
 
 import (
+	"os"
+
+	. "github.com/mandelsoft/goutils/testutils"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	. "github.com/open-component-model/ocm/cmds/ocm/testhelper"
+
+	"github.com/open-component-model/ocm/pkg/common/accessio"
+	"github.com/open-component-model/ocm/pkg/contexts/oci/attrs/cacheattr"
 )
 
-var _ = Describe("OCM command line test Environment (!windows)", func() {
+var _ = Describe("OCM command line test Environment", func() {
 	var env *TestEnv
 
 	BeforeEach(func() {
@@ -20,9 +24,14 @@ var _ = Describe("OCM command line test Environment (!windows)", func() {
 	})
 
 	It("Add OCI image resource fails when a cache directory is specified on Windows", func() {
+		tmp := Must(os.MkdirTemp("", "ocm-cache-*"))
+		defer os.RemoveAll(tmp)
+		// configure ocm cache
+		MustBeSuccessful(cacheattr.Set(env.OCIContext(), Must(accessio.NewStaticBlobCache(tmp))))
+
 		// ocm create ca --file ca --scheme ocm.software/v3alpha1 --provider test.com test.com/postgresql 14.0.5
 		Expect(env.Execute("create", "ca", "--file", "ca", "--scheme", "ocm.software/v3alpha1", "--provider", "test.com", "test.com/postgresql", "14.0.5")).To(Succeed())
 		// ocm add resource --file ca --name bitnami-shell --type ociImage --accessType ociArtifact --version 10 --reference bitnami/postgresql:16.2.0-debian-11-r1
-		Expect(env.Execute("--config", "/testdata/ocmconfig-cache.yaml", "add", "resource", "--file", "ca", "--name", "bitnami-shell", "--type", "ociImage", "--accessType", "ociArtifact", "--version", "10", "--reference", "bitnami/postgresql:16.2.0-debian-11-r1")).To(Succeed())
+		Expect(env.Execute("add", "resource", "--file", "ca", "--name", "bitnami-shell", "--type", "ociImage", "--accessType", "ociArtifact", "--version", "10", "--reference", "bitnami/postgresql:16.2.0-debian-11-r1")).To(Succeed())
 	})
 })
