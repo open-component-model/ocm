085fd526d4848904d4d49e07120524fbe9cd8a3b
Uwe Krueger <uwe.krueger@sap.com>
2024-07-17 21:44:25 +0200

    rework selectors

--- pkg/contexts/ocm/compdesc/selectors.go
+++ pkg/contexts/ocm/compdesc/selectors.go
@@ -12,6 +12,7 @@ import (
 	"github.com/open-component-model/ocm/pkg/utils/selector"
 )
 
+// Deprecated: use package selectors and its sub packages.
 type IdentityBasedSelector interface {
 	IdentitySelector
 	ElementSelector
@@ -19,12 +20,14 @@ type IdentityBasedSelector interface {
 	ReferenceSelector
 }
 
+// Deprecated: use package selectors and its sub packages.
 type ElementBasedSelector interface {
 	ElementSelector
 	ResourceSelector
 	ReferenceSelector
 }
 
+// Deprecated: use package selectors and its sub packages.
 type LabelBasedSelector interface {
 	LabelSelector
 	ElementSelector
@@ -34,6 +37,7 @@ type LabelBasedSelector interface {
 
 ////////////////////////////////////////////////////////////////////////////////
 
+// Deprecated: use package selectors and its sub packages.
 type IdentitySelector = selector.Interface
 
 type byVersion struct {
@@ -60,6 +64,8 @@ func (b *byVersion) MatchReference(obj ReferenceSelectionContext) (bool, error) 
 
 // ByVersion creates a new selector that
 // selects an element based on its version.
+//
+// Deprecated: use package selectors and its sub packages.
 func ByVersion(version string) IdentityBasedSelector {
 	return &byVersion{version: version}
 }
@@ -88,6 +94,8 @@ func (b *byName) MatchReference(obj ReferenceSelectionContext) (bool, error) {
 
 // ByName creates a new selector that
 // selects an element based on its name.
+//
+// Deprecated: use package selectors and its sub packages.
 func ByName(name string) IdentityBasedSelector {
 	return &byName{name: name}
 }
@@ -126,6 +134,8 @@ func (b *byIdentity) MatchReference(obj ReferenceSelectionContext) (bool, error)
 
 // ByIdentity creates a new resource and identity selector that
 // selects a resource based on its identity.
+//
+// Deprecated: use package selectors and its sub packages.
 func ByIdentity(name string, extras ...string) IdentityBasedSelector {
 	id := v1.NewIdentity(name, extras...)
 	return &byIdentity{id: id}
@@ -135,6 +145,8 @@ func ByIdentity(name string, extras ...string) IdentityBasedSelector {
 // selects a resource based on its partial identity.
 // All given attributes must match, but potential additional attributes
 // of a resource identity are ignored.
+//
+// Deprecated: use package selectors and its sub packages.
 func ByPartialIdentity(name string, extras ...string) IdentityBasedSelector {
 	id := v1.NewIdentity(name, extras...)
 	return &byIdentity{id: id, partial: true}
@@ -172,6 +184,8 @@ func (b *withExtraId) MatchReference(obj ReferenceSelectionContext) (bool, error
 
 // WithExtraIdentity creates a new selector that
 // selects an element based on extra identities.
+//
+// Deprecated: use package selectors and its sub packages.
 func WithExtraIdentity(args ...string) IdentityBasedSelector {
 	ids := v1.Identity{}
 	for i := 0; i < len(args); i += 2 {
@@ -185,6 +199,8 @@ func WithExtraIdentity(args ...string) IdentityBasedSelector {
 ////////////////////////////////////////////////////////////////////////////////
 
 // ResourceSelectorFunc defines a function to filter a resource.
+//
+// Deprecated: use package selectors and its sub packages.
 type ResourceSelectorFunc func(obj ResourceSelectionContext) (bool, error)
 
 var _ ResourceSelector = ResourceSelectorFunc(nil)
@@ -198,6 +214,7 @@ type resourceSelectionContext struct {
 	identity
 }
 
+// Deprecated: use package selectors and its sub packages.
 func NewResourceSelectionContext(index int, rscs Resources) ResourceSelectionContext {
 	return &resourceSelectionContext{
 		Resource: &rscs[index],
@@ -211,14 +228,20 @@ func NewResourceSelectionContext(index int, rscs Resources) ResourceSelectionCon
 // ResourceSelectionContext describes the selction context for a resource
 // selector. It contains the resource and provides access to its
 // identity in the context of its component descriptor.
+//
+// Deprecated: use package selectors and its sub packages.
 type ResourceSelectionContext = *resourceSelectionContext
 
 // ResourceSelector defines a selector based on resource attributes.
+//
+// Deprecated: use package selectors and its sub packages.
 type ResourceSelector interface {
 	MatchResource(obj ResourceSelectionContext) (bool, error)
 }
 
 // MatchResourceByResourceSelector applies all resource selector against the given resource object.
+//
+// Deprecated: use package selectors and its sub packages.
 func MatchResourceByResourceSelector(obj ResourceSelectionContext, resourceSelectors ...ResourceSelector) (bool, error) {
 	for _, sel := range resourceSelectors {
 		ok, err := sel.MatchResource(obj)
@@ -233,6 +256,8 @@ func MatchResourceByResourceSelector(obj ResourceSelectionContext, resourceSelec
 }
 
 // AndR is an AND resource selector.
+//
+// Deprecated: use package labelsel.
 func AndR(sel ...ResourceSelector) ResourceSelector {
 	return ResourceSelectorFunc(func(obj ResourceSelectionContext) (bool, error) {
 		for _, s := range sel {
@@ -246,6 +271,8 @@ func AndR(sel ...ResourceSelector) ResourceSelector {
 }
 
 // OrR is an OR resource selector.
+//
+// Deprecated: use package labelsel.
 func OrR(sel ...ResourceSelector) ResourceSelector {
 	return ResourceSelectorFunc(func(obj ResourceSelectionContext) (bool, error) {
 		for _, s := range sel {
@@ -259,6 +286,8 @@ func OrR(sel ...ResourceSelector) ResourceSelector {
 }
 
 // NotR is a negated resource selector.
+//
+// Deprecated: use package labelsel.
 func NotR(sel ResourceSelector) ResourceSelector {
 	return ResourceSelectorFunc(func(obj ResourceSelectionContext) (bool, error) {
 		ok, err := sel.MatchResource(obj)
@@ -271,6 +300,8 @@ func NotR(sel ResourceSelector) ResourceSelector {
 
 // ByResourceType creates a new resource selector that
 // selects a resource based on its type.
+//
+// Deprecated: use package selectors and its sub packages.
 func ByResourceType(ttype string) ResourceSelector {
 	return ResourceSelectorFunc(func(obj ResourceSelectionContext) (bool, error) {
 		return ttype == "" || obj.GetType() == ttype, nil
@@ -279,6 +310,8 @@ func ByResourceType(ttype string) ResourceSelector {
 
 // ByRelation creates a new resource selector that
 // selects a resource based on its relation type.
+//
+// Deprecated: use package selectors and its sub packages.
 func ByRelation(relation v1.ResourceRelation) ResourceSelectorFunc {
 	return ResourceSelectorFunc(func(obj ResourceSelectionContext) (bool, error) {
 		return obj.Relation == relation, nil
@@ -286,6 +319,8 @@ func ByRelation(relation v1.ResourceRelation) ResourceSelectorFunc {
 }
 
 // ByAccessMethod creates a new selector that matches a resource access method type.
+//
+// Deprecated: use package selectors and its sub packages.
 func ByAccessMethod(name string) ResourceSelector {
 	return ResourceSelectorFunc(func(obj ResourceSelectionContext) (bool, error) {
 		if obj.Access == nil {
@@ -296,6 +331,8 @@ func ByAccessMethod(name string) ResourceSelector {
 }
 
 // ForExecutable creates a new selector that matches a resource for an executable.
+//
+// Deprecated: use package selectors and its sub packages.
 func ForExecutable(name string) ResourceSelector {
 	return ResourceSelectorFunc(func(obj ResourceSelectionContext) (bool, error) {
 		return obj.Name == name && obj.Type == resourcetypes.EXECUTABLE && obj.ExtraIdentity != nil &&
@@ -307,11 +344,15 @@ func ForExecutable(name string) ResourceSelector {
 ////////////////////////////////////////////////////////////////////////////////
 
 // LabelSelector is used to match a label in a label set.
+//
+// Deprecated: use package selectors and its sub packages.
 type LabelSelector interface {
 	MatchLabel(l v1.Label) (bool, error)
 }
 
 // LabelSelectorFunc is a function used as LabelSelector.
+//
+// Deprecated: use package selectors and its sub packages.
 type LabelSelectorFunc func(l v1.Label) (bool, error)
 
 func (l LabelSelectorFunc) MatchLabel(label v1.Label) (bool, error) {
@@ -319,6 +360,8 @@ func (l LabelSelectorFunc) MatchLabel(label v1.Label) (bool, error) {
 }
 
 // AndL is an AND label selector.
+//
+// Deprecated: use package selectors and its sub packages.
 func AndL(sel ...LabelSelector) LabelSelector {
 	return LabelSelectorFunc(func(obj v1.Label) (bool, error) {
 		for _, s := range sel {
@@ -332,6 +375,8 @@ func AndL(sel ...LabelSelector) LabelSelector {
 }
 
 // OrL is an OR label selector.
+//
+// Deprecated: use package selectors and its sub packages.
 func OrL(sel ...LabelSelector) LabelSelector {
 	return LabelSelectorFunc(func(obj v1.Label) (bool, error) {
 		for _, s := range sel {
@@ -345,6 +390,8 @@ func OrL(sel ...LabelSelector) LabelSelector {
 }
 
 // NotL is a negated label selector.
+//
+// Deprecated: use package selectors and its sub packages.
 func NotL(sel LabelSelector) LabelSelector {
 	return LabelSelectorFunc(func(obj v1.Label) (bool, error) {
 		ok, err := sel.MatchLabel(obj)
@@ -392,6 +439,8 @@ func (b *byLabel) MatchLabel(l v1.Label) (bool, error) {
 // be grouped into a single label selector to be applied in
 // combination. Otherwise, a resource might match if the label
 // selectors all match, but different labels.
+//
+// Deprecated: use package selectors and its sub packages.
 func ByLabel(sel ...LabelSelector) LabelBasedSelector {
 	return &byLabel{selector: LabelSelectorFunc(func(l v1.Label) (bool, error) {
 		return MatchLabels(v1.Labels{l}, sel...)
@@ -399,12 +448,16 @@ func ByLabel(sel ...LabelSelector) LabelBasedSelector {
 }
 
 // ByLabelName matches an element by a label name.
+//
+// Deprecated: use package selectors and its sub packages.
 func ByLabelName(name string) LabelBasedSelector {
 	return &byLabel{selector: LabelSelectorFunc(func(l v1.Label) (bool, error) { return l.Name == name, nil })}
 }
 
 // ByLabelValue matches a resource or label by a label value.
 // This selector should typically be combined with ByLabelName.
+//
+// Deprecated: use package selectors and its sub packages.
 func ByLabelValue(value interface{}) LabelBasedSelector {
 	return &byLabel{selector: LabelSelectorFunc(func(l v1.Label) (bool, error) {
 		var data interface{}
@@ -417,18 +470,24 @@ func ByLabelValue(value interface{}) LabelBasedSelector {
 
 // ByLabelVersion matches a resource or label by a label version.
 // This selector should typically be combined with ByLabelName.
+//
+// Deprecated: use package selectors and its sub packages.
 func ByLabelVersion(version string) LabelBasedSelector {
 	return &byLabel{selector: LabelSelectorFunc(func(l v1.Label) (bool, error) { return l.Version == version, nil })}
 }
 
 // BySignedLabel matches a resource or label by a label indicated to be signed.
 // This selector should typically be combined with ByLabelName.
+//
+// Deprecated: use package selectors and its sub packages.
 func BySignedLabel(flags ...bool) LabelBasedSelector {
 	flag := utils.OptionalDefaultedBool(true, flags...)
 	return &byLabel{selector: LabelSelectorFunc(func(l v1.Label) (bool, error) { return l.Signing == flag, nil })}
 }
 
 // MatchLabels checks whether a set of labels matches the given label selectors.
+//
+// Deprecated: use package selectors and its sub packages.
 func MatchLabels(labels v1.Labels, sel ...LabelSelector) (bool, error) {
 	if len(labels) == 0 && len(sel) == 0 {
 		return true, nil
@@ -453,6 +512,8 @@ outer:
 }
 
 // SelectLabels returns labels matching the given label selectors.
+//
+// Deprecated: use package selectors and its sub packages.
 func SelectLabels(labels v1.Labels, sel ...LabelSelector) (v1.Labels, error) {
 	list := make(v1.Labels, 0)
 outer:
@@ -473,6 +534,8 @@ outer:
 }
 
 // MatchReferencesByReferenceSelector applies all resource selector against the given resource object.
+//
+// Deprecated: use package selectors and its sub packages.
 func MatchReferencesByReferenceSelector(obj ReferenceSelectionContext, resourceSelectors ...ReferenceSelector) (bool, error) {
 	for _, sel := range resourceSelectors {
 		ok, err := sel.MatchReference(obj)
@@ -493,8 +556,10 @@ type elementSelectionContext struct {
 	identity
 }
 
+// Deprecated: not supported anymore.
 type ElementSelectionContext = *elementSelectionContext
 
+// Deprecated: not supported anymore.
 func NewElementSelectionContext(index int, elems ElementAccessor) ElementSelectionContext {
 	return &elementSelectionContext{
 		ElementMeta: elems.Get(index).GetMeta(),
@@ -505,6 +570,7 @@ func NewElementSelectionContext(index int, elems ElementAccessor) ElementSelecti
 	}
 }
 
+// Deprecated: use package selectors and its sub packages.
 type ElementSelector interface {
 	MatchElement(obj ElementSelectionContext) (bool, error)
 }
@@ -512,6 +578,8 @@ type ElementSelector interface {
 ////////////////////////////////////////////////////////////////////////////////
 
 // ReferenceSelectorFunc defines a function to filter a resource.
+//
+// Deprecated: use package selectors and its sub packages.
 type ReferenceSelectorFunc func(obj ReferenceSelectionContext) (bool, error)
 
 var _ ReferenceSelector = ReferenceSelectorFunc(nil)
@@ -525,6 +593,7 @@ type referenceSelectionContext struct {
 	identity
 }
 
+// Deprecated: use package selectors and its sub packages.
 func NewReferenceSelectionContext(index int, refs References) ReferenceSelectionContext {
 	return &referenceSelectionContext{
 		ComponentReference: &refs[index],
@@ -538,14 +607,20 @@ func NewReferenceSelectionContext(index int, refs References) ReferenceSelection
 // ReferenceSelectionContext describes the selection context for a reference
 // selector. It contains the reference and provides access to its
 // identity in the context of its component descriptor.
+//
+// Deprecated: use package selectors and its sub packages.
 type ReferenceSelectionContext = *referenceSelectionContext
 
 // ReferenceSelector defines a selector based on reference attributes.
+//
+// Deprecated: use package selectors and its sub packages.
 type ReferenceSelector interface {
 	MatchReference(obj ReferenceSelectionContext) (bool, error)
 }
 
 // AndC is an AND reference selector.
+//
+// Deprecated: use package selectors and its sub packages.
 func AndC(sel ...ReferenceSelector) ReferenceSelector {
 	return ReferenceSelectorFunc(func(obj ReferenceSelectionContext) (bool, error) {
 		for _, s := range sel {
@@ -559,6 +634,8 @@ func AndC(sel ...ReferenceSelector) ReferenceSelector {
 }
 
 // OrC is an OR resource selector.
+//
+// Deprecated: use package selectors and its sub packages.
 func OrC(sel ...ReferenceSelector) ReferenceSelector {
 	return ReferenceSelectorFunc(func(obj ReferenceSelectionContext) (bool, error) {
 		for _, s := range sel {
@@ -572,6 +649,8 @@ func OrC(sel ...ReferenceSelector) ReferenceSelector {
 }
 
 // NotC is a negated resource selector.
+//
+// Deprecated: use package selectors and its sub packages.
 func NotC(sel ReferenceSelector) ReferenceSelector {
 	return ReferenceSelectorFunc(func(obj ReferenceSelectionContext) (bool, error) {
 		ok, err := sel.MatchReference(obj)
@@ -582,6 +661,7 @@ func NotC(sel ReferenceSelector) ReferenceSelector {
 	})
 }
 
+// Deprecated: use package selectors and its sub packages.
 func ByComponent(name string) ReferenceSelector {
 	return ReferenceSelectorFunc(func(obj ReferenceSelectionContext) (bool, error) {
 		return obj.ComponentName == name, nil
---
+++ pkg/contexts/ocm/selectors/labelsel/suite_test.go
@@ -0,0 +1,13 @@
+package labelsel_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestConfig(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "label selectorTest Suite")
+}
