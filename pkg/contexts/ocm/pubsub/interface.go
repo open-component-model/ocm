9d9e6dd559d50103d1b6bfbb0f74f353e62ab7c3
Uwe Krueger <uwe.krueger@sap.com>
2024-07-26 14:31:12 +0200

    Update pkg/contexts/ocm/pubsub/interface.go
    Co-authored-by: Fabian Burth <fabian.burth@sap.com>

--- pkg/contexts/ocm/pubsub/interface.go
+++ pkg/contexts/ocm/pubsub/interface.go
@@ -58,7 +58,7 @@ type PubSubMethod interface {
 }
 
 // TypeScheme is the registry for specification types for
-// Pub Sub types. A PubSub types is finally able to
+// PubSub types. A PubSub type is finally able to
 // provide an implementation for notifying a dedicated
 // Pub Sub instance.
 type TypeScheme descriptivetype.TypeScheme[PubSubSpec, PubSubType]
