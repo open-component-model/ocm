d96ed390832c0205937d2fc7bf3900c53fd1e8fc
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2024-10-25 08:48:11 +0200

    make format

--- cmds/ocm/commands/ocmcmds/components/hash/cmd_test.go
+++ cmds/ocm/commands/ocmcmds/components/hash/cmd_test.go
@@ -7,12 +7,12 @@ import (
 	. "github.com/mandelsoft/goutils/testutils"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"ocm.software/ocm/api/ocm/extensions/repositories/ctf"
 	. "ocm.software/ocm/cmds/ocm/testhelper"
 
 	"ocm.software/ocm/api/ocm/compdesc"
 	metav1 "ocm.software/ocm/api/ocm/compdesc/meta/v1"
 	resourcetypes "ocm.software/ocm/api/ocm/extensions/artifacttypes"
+	"ocm.software/ocm/api/ocm/extensions/repositories/ctf"
 	"ocm.software/ocm/api/utils/accessio"
 	"ocm.software/ocm/api/utils/mime"
 )
