932252deb55adc0ed75e8ac0cd8a658bf6275455
jakobmoellerdev <jakob.moeller@sap.com>
2024-11-20 19:00:52 +0100

    chore: log http requests on trace level by injecting a logger
    it is now possible to inject a trace attribute to OCMs logging architecture to allow tracing back HTTP calls for oci registries and the docker client:
    
    ```
    ocm --logkeys /+ocm/oci=trace [YOUR COMMAND INTERACTING WITH OCI HERE]
    ```
    
    Note that this does not take care of all access types yet because we dont have a unified http.Client

--- api/oci/extensions/repositories/ocireg/logging.go
+++ api/oci/extensions/repositories/ocireg/logging.go
@@ -1,7 +1,5 @@
 package ocireg
 
-import (
-	ocmlog "ocm.software/ocm/api/utils/logging"
-)
+import ocmlog "ocm.software/ocm/api/utils/logging"
 
 var REALM = ocmlog.DefineSubRealm("OCI repository handling", "oci", "ocireg")
