761c5aae03cbb4c2d25b72c181bb63c4079d6039
Uwe Krueger <uwe.krueger@sap.com>
2024-11-22 11:05:07 +0100

    provide dedicated decision factories

--- cmds/transferplugin/transferhandlers/demo.go
+++ cmds/transferplugin/transferhandlers/demo.go
@@ -3,7 +3,6 @@ package transferhandlers
 import (
 	"slices"
 
-	"github.com/mandelsoft/goutils/general"
 	"github.com/mandelsoft/goutils/optionutils"
 	"ocm.software/ocm/api/ocm/plugin/ppi"
 	"ocm.software/ocm/cmds/transferplugin/config"
@@ -15,51 +14,41 @@ const (
 
 func New() ppi.TransferHandler {
 	h := ppi.NewTransferHandler(NAME, "enable value transport for dedicated external repositories")
-	h.RegisterDecision(NewDecision(ppi.Q_TRANSFER_RESOURCE, func(options *ppi.TransferOptions) bool { return optionutils.AsBool(options.ResourcesByValue) },
-		`value transport only for dedicated access types and service hosts`))
-	h.RegisterDecision(NewDecision(ppi.Q_TRANSFER_SOURCE, func(options *ppi.TransferOptions) bool { return optionutils.AsBool(options.SourcesByValue) },
-		`value transport only for dedicated access types and service hosts`))
+
+	h.RegisterDecision(ppi.NewTransferResourceDecision(`value transport only for dedicated access types and service hosts`,
+		ForOptions(func(options *ppi.TransferOptions) bool { return optionutils.AsBool(options.ResourcesByValue) })))
+
+	h.RegisterDecision(ppi.NewTransferSourceDecision(`value transport only for dedicated access types and service hosts`,
+		ForOptions(func(options *ppi.TransferOptions) bool { return optionutils.AsBool(options.SourcesByValue) })))
 	return h
 }
 
-type DecisionHandler struct {
-	ppi.DecisionHandlerBase
-	optfunc func(opts *ppi.TransferOptions) bool
-}
+type OptionFunc func(opts *ppi.TransferOptions) bool
 
-var _ ppi.DecisionHandler = (*DecisionHandler)(nil)
+func ForOptions(f OptionFunc) ppi.ArtifactQuestionFunc {
+	return func(p ppi.Plugin, question *ppi.ArtifactQuestionArguments) (bool, error) {
+		var cfg *config.Config
 
-func NewDecision(typ string, optfunc func(opts *ppi.TransferOptions) bool, desc ...string) ppi.DecisionHandler {
-	return &DecisionHandler{
-		DecisionHandlerBase: ppi.NewDecisionHandlerBase(typ, general.Optional(desc...)),
-		optfunc:             optfunc,
-	}
-}
-
-func (d DecisionHandler) DecideOn(p ppi.Plugin, question ppi.QuestionArguments) (bool, error) {
-	q := question.(*ppi.ArtifactQuestionArguments)
-
-	var cfg *config.Config
-
-	if q.Options.Special == nil {
-		c, err := p.GetConfig()
-		if c == nil || err != nil {
-			return false, err
-		}
-		cfg = c.(*config.Config)
-	} else {
-		c, err := config.GetConfig(*q.Options.Special)
-		if err != nil {
-			return false, err
-		}
-		if c != nil {
+		if question.Options.Special == nil {
+			c, err := p.GetConfig()
+			if c == nil || err != nil {
+				return false, err
+			}
 			cfg = c.(*config.Config)
+		} else {
+			c, err := config.GetConfig(*question.Options.Special)
+			if err != nil {
+				return false, err
+			}
+			if c != nil {
+				cfg = c.(*config.Config)
+			}
 		}
-	}
 
-	if list := cfg.TransferRepositories.Types[q.Artifact.AccessInfo.Kind]; list != nil {
-		host := q.Artifact.AccessInfo.Host
-		return slices.Contains(list, host), nil
+		if list := cfg.TransferRepositories.Types[question.Artifact.AccessInfo.Kind]; list != nil {
+			host := question.Artifact.AccessInfo.Host
+			return slices.Contains(list, host), nil
+		}
+		return f(&question.Options), nil
 	}
-	return d.optfunc(&q.Options), nil
 }
