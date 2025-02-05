905c5f1d99d6e2268e8c15333f6dd72d558bd3e8
Fabian Burth <fabian.burth@sap.com>
2025-02-05 14:26:33 +0100

    chore: remove header information log

--- api/utils/logging/roundtripper.go
+++ api/utils/logging/roundtripper.go
@@ -20,19 +20,9 @@ type RoundTripper struct {
 }
 
 func (t *RoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
-	// Redact the Authorization header to make sure it doesn't get logged at any point.
-	header := req.Header.Clone()
-	sensitiveHeaders := []string{"Authorization", "Cookie", "Set-Cookie", "Proxy-Authorization", "WWW-Authenticate"}
-	for _, key := range sensitiveHeaders {
-		if header.Get(key) != "" {
-			header.Set(key, "***")
-		}
-	}
-
 	t.logger.Trace("roundtrip",
 		"url", req.URL,
 		"method", req.Method,
-		"header", header,
 	)
 	return t.RoundTripper.RoundTrip(req)
 }
