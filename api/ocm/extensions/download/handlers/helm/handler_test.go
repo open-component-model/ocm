4b3b8ddbd545ecf889906806ea3aa6f807a87e5e
Frederic Wilhelm <frederic.wilhelm@sap.com>
2025-05-28 16:28:50 +0200

    chore: fix correct filename for provenance file (#1492)
    According to the helm
    [spec](https://github.com/helm/community/blob/05eab5bba990d4f5556f90bfc001662d1a76f9a5/hips/hip-0006.md#2-support-for-provenance-files)
    
    > assuming the presence of a file suffixed with .prov sitting next to a
    chart .tgz in a repository.
    
    This is why, we initially downloaded the provenance file into a file
    with only the `.prov` extension. However, according to the [helm
    documentation](https://github.com/helm/helm-www/blob/cf3e4f875101fa3f72ff6194499b5365723f0ec1/content/en/docs/topics/provenance.md)
    
    > For example, if the base URL for a package is
    https://example.com/charts/mychart-1.2.3.tgz, the provenance file, if it
    exists, MUST be accessible at
    https://example.com/charts/mychart-1.2.3.tgz.prov.
    
    Accordingly, we changed the filename for the provenance content to
    include the extensions of the original helm-chart
    
    (cherry picked from commit ab3c2d955eaaadc09e0df6436a3c88db335ce22a)

--- api/ocm/extensions/download/handlers/helm/handler_test.go
+++ api/ocm/extensions/download/handlers/helm/handler_test.go
@@ -22,24 +22,3 @@ func TestAssureArchiveSuffix(t *testing.T) {
 		}
 	}
 }
-
-func TestTrimExtN(t *testing.T) {
-	tests := []struct {
-		input    string
-		n        int
-		expected string
-	}{
-		{"chart.tgz", 1, "chart"},
-		{"chart.tar.gz", 1, "chart.tar"},
-		{"chart.tar.gz", 2, "chart"},
-		{"archive.zip", 1, "archive"},
-		{"archive", 1, "archive"},
-	}
-
-	for _, tt := range tests {
-		result := trimExtN(tt.input, tt.n)
-		if result != tt.expected {
-			t.Errorf("trimExtN(%q, %d) = %q; want %q", tt.input, tt.n, result, tt.expected)
-		}
-	}
-}
