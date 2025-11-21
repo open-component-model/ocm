b816038a76e0d9104e2ee2d0b1d3f4f6f9bfb0aa
d.small@sap.com <d.small@sap.com>
2025-11-21 03:49:55 +0000

    Correct the usage of sync.OnceFunc
    Signed-off-by: d.small@sap.com <d.small@sap.com>

--- api/utils/yqlib/init.go
+++ api/utils/yqlib/init.go
@@ -10,28 +10,30 @@ import (
 	ocmlog "ocm.software/ocm/api/utils/logging"
 )
 
+var yqinit = sync.OnceFunc(func() {
+	var lvl glog.Level
+	switch ocmlog.Context().GetDefaultLevel() {
+	case mlog.None:
+		fallthrough
+	case mlog.ErrorLevel:
+		lvl = glog.ERROR
+	case mlog.WarnLevel:
+		lvl = glog.WARNING
+	case mlog.InfoLevel:
+		lvl = glog.INFO
+	case mlog.DebugLevel:
+		fallthrough
+	case mlog.TraceLevel:
+		lvl = glog.DEBUG
+	}
+	glog.SetLevel(lvl, "yq-lib")
+
+	yqlib.InitExpressionParser()
+})
+
 func InitYq() {
 	// 2025-11-19 d :
 	// Prevent https://github.com/open-component-model/ocm-project/issues/773
 	// Race condition with the setting of the log level 'yq-lib'
-	sync.OnceFunc(func() {
-		var lvl glog.Level
-		switch ocmlog.Context().GetDefaultLevel() {
-		case mlog.None:
-			fallthrough
-		case mlog.ErrorLevel:
-			lvl = glog.ERROR
-		case mlog.WarnLevel:
-			lvl = glog.WARNING
-		case mlog.InfoLevel:
-			lvl = glog.INFO
-		case mlog.DebugLevel:
-			fallthrough
-		case mlog.TraceLevel:
-			lvl = glog.DEBUG
-		}
-		glog.SetLevel(lvl, "yq-lib")
-
-		yqlib.InitExpressionParser()
-	})()
+	yqinit()
 }
